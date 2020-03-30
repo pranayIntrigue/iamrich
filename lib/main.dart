dab7531cb168e5b79babf01bb902f41baa76293b
pranayIntrigue <pranay@intrigueme.ca>
2020-03-30 11:03:05 -0400

    Init App using flutter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://79qmx83o1r-flywheel.netdna-ssl.com/wp-content/uploads/2018/07/intrigue-logo-white-1.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
